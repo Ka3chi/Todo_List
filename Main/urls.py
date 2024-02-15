0c46c257d54c6cae2592dc06528721a28d1e5624
Ka3chi <aldrinalmoradie69@gmail.com>
2024-02-15 14:39:24 +0800

    login

--- Main/urls.py
+++ Main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import tasklist, taskdetail, taskcreate, taskupdate, taskdelete, loginview
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login/', loginview.as_view(), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('', tasklist.as_view(), name='tasks'),
     path('task/<int:pk>/', taskdetail.as_view(), name='task'),
     path('task-create/', taskcreate.as_view(), name='task-create'),
--- Main/views.py
+++ Main/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import task
 
 # Create your views here.
@@ -16,28 +17,28 @@ class loginview(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class tasklist(ListView):
+class tasklist(LoginRequiredMixin, ListView):
     model = task
     context_object_name = 'tasks'
 
-class taskdetail(DetailView):
+class taskdetail(LoginRequiredMixin, DetailView):
     model = task
     context_object_name = 'task'
     template_name = 'Main/task.html'
 
-class taskcreate(CreateView):
+class taskcreate(LoginRequiredMixin, CreateView):
     model = task
     template_name = 'Main/taskform.html'
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class taskupdate(UpdateView):
+class taskupdate(LoginRequiredMixin, UpdateView):
     model = task
     template_name = 'Main/taskform.html'
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class taskdelete(DeleteView):
+class taskdelete(LoginRequiredMixin, DeleteView):
     model = task
     template_name = 'Main/taskdelete.html'
     context_object_name = 'tasks'
