92a00b53ee4b7148302f356ac897ff8855c24cfa
Ka3chi <aldrinalmoradie69@gmail.com>
2024-02-21 16:00:09 +0800

    done registration

--- Main/urls.py
+++ Main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import tasklist, taskdetail, taskcreate, taskupdate, taskdelete, loginview
+from .views import tasklist, taskdetail, taskcreate, taskupdate, taskdelete, loginview, register
 from . import views
 
 urlpatterns = [
     path('login/', loginview.as_view(), name='login'),
+    path('register/', register.as_view(), name='register'),
     path('logout/', views.LogOut, name='logout'),
     path('', tasklist.as_view(), name='tasks'),
     path('task/<int:pk>/', taskdetail.as_view(), name='task'),
--- Main/views.py
+++ Main/views.py
@@ -1,11 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 from .models import task
 
 # Create your views here.
@@ -21,6 +26,21 @@ class loginview(LoginView):
     
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class register(FormView):
+    template_name = 'Main/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('login')
+    
+    def form_valid(self, form):
+        # Save the user
+        user = form.save()
+        
+        if user is not None:
+            login(self.request, user)
+        
+        return super().form_valid(form)
 
 class tasklist(LoginRequiredMixin, ListView):
     model = task
